d9973cea7a1c59cbe52e75359944f52624e26338
Sean Dantes <122073477+sean207@users.noreply.github.com>
2025-07-13 13:37:18 -0700

    Update randomCode.java

--- randomCode.java
+++ randomCode.java
@@ -1,1 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.randomcode;
+
+/**
+ *
+ * @author PC
+ */
+public class RandomCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
 
